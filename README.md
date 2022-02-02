5c88364fd4ac714325d0634396a680bfb3a087f7
UneBaguette <thomasiger42@gmail.com>
2022-02-02 16:40:42 +0100

    Testing in progress ;)

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # E-commerce product page
 
+### Projet de groupe consistant à reproduire un site de E-commerce.
 
-<p>Authored-by: Thomas</p>
-<p>Co-authored-by: Erwan Brévault</p>
+## Thomas
+
+### J'ai commencé par m'occuper d'une grande partie de la mise en page, du compteur de produits, ensuite de la création du panier d'achat et enfin de la lightbox.
+
+## Erwan
+
+### Ma tâche était de m'occuper de la partie Mobile.
+
+### Tout d'abord je devais afficher un bouton burger ainsi que son contenu, ensuite créer le carrousel d'images et enfin de réadapter les titres et la description du produit.
--- script.js
+++ script.js
@@ -1,62 +1,93 @@
 // déclaration variables et constantes
-const carrousel = document.querySelectorAll('.mobile img');
-const suivant = document.querySelector('.right');
-const precedent = document.querySelector('.left');
+const carrouselMobile = document.querySelectorAll('.mobile img');
+const suivantMobile = document.querySelector('.right');
+const precedentMobile = document.querySelector('.left');
 const mobileNav = document.querySelector(".mobile-nav");
 const burger = document.querySelector(".btn-burger");
 const burger2 = document.querySelector(".btn-burger2");
+const overlay = document.querySelector('.ov');
 const contactBtn = document.querySelector(".btn");
 const cart = document.querySelector('.cart');
 const buttons = document.querySelectorAll('.number > img');
 const display = document.querySelector('.number > span');
 const shop = document.querySelector('.shopping');
 const main = document.querySelectorAll('.main-img');
-const thumbnail = document.querySelectorAll('.thumbnail > img')
+const thumbnail = document.querySelectorAll('.thumbnail > img');
 const lightbox = document.querySelector('.lightbox');
+const lbimg = document.querySelectorAll('.lb-img');
 const exit = document.querySelector('.close-btn');
+const suivantDesktop = document.querySelector('.right-lb');
+const precedentDesktop = document.querySelector('.left-lb');
 
 // Bouton menu mobile
 // ouverture bouton mobile 
 burger.addEventListener("click", () => {
-  mobileNav.classList.toggle("mobile-nav-active");
-  burger.classList.toggle("toggle");
+    mobileNav.classList.toggle("mobile-nav-active");
+    overlayCheck();
+    burger.classList.toggle("toggle");
 });
 // fermeture bouton mobile
 burger2.addEventListener("click", () => {
     mobileNav.classList.toggle("mobile-nav-active");
+    overlayCheck();
     burger2.classList.toggle("toggle");
 });
 
+function overlayCheck(){
+    if (mobileNav.classList.contains('mobile-nav-active')) {
+        return overlay.classList.add('active');
+    }
+    else{
+        return overlay.classList.remove('active');
+    }
+}
 
 // Gallery 
-
 main.forEach(mainimg => {
-    mainimg.addEventListener('click', () => {
-        if (lightbox.classList.contains('active-lb')) {
-            lightbox.classList.remove('active-lb');
-        }
-        else {
-            lightbox.classList.add('active-lb');
+    mainimg.addEventListener('click', checkLB);
+})
+
+exit.addEventListener('click', checkLB);
+
+function checkLB(){
+    if (lightbox.classList.contains('active-lb')) {
+        return lightbox.classList.remove('active-lb');
+    }
+    else {
+        return lightbox.classList.add('active-lb');
+    }
+}
+
+thumbnail.forEach(thumb => {
+    let index = 0;
+    thumb.addEventListener('click', () => {
+        console.log(lbimg)
+        index = thumb.getAttribute('data-ac');
+        for(let i = 0; i < thumbnail.length; i++){
+            if (thumbnail[i].getAttribute('data-ac') === index){
+                thumbnail[i].classList.add('active-1');
+                main[i].classList.add('active');
+                lbimg[i].classList.add('active');
+            }
+            else {
+                thumbnail[i].classList.remove('active-1');
+                main[i].classList.remove('active');
+                lbimg[i].classList.remove('active');
+            }
         }
     })
 })
 
-exit.addEventListener('click', () => {
-    if (lightbox.classList.contains('active-lb')) {
-        lightbox.classList.remove('active-lb');
-    }
-    else {
-        lightbox.classList.add('active-lb');
-    }
-})
 
 
 // Shopping cart - Show/Hide
 cart.addEventListener('click', () => {
     if (shop.classList.contains('active')) {
+        cart.classList.remove('active-cart');
         shop.classList.remove('active');
     }
     else {
+        cart.classList.add('active-cart');
         shop.classList.add('active');
     }
 });
@@ -81,41 +112,48 @@ buttons.forEach(button => {
     });
 })
 
-// Carousel
+// Carrousel MOBILE
 
 // declaration d'une variable index = 0 pour definir l'emplacement du 1er element dans le tableau
 let index = 0;
 // avancer dans le carrousel
-suivant.addEventListener('click', slideSuivante);
+suivantMobile.addEventListener('click', slideSuivante);
 
 function slideSuivante(){
     // recuperation de la 1ere image
-    if (index<3){
-        carrousel[index].classList.remove('active');
+    if (index < 3){
+        carrouselMobile[index].classList.remove('active');
         index++;
-        carrousel[index].classList.add('active');
+        carrouselMobile[index].classList.add('active');
     }
     //  retourner au debut du tableau
     else if(index === 3){
-        carrousel[index].classList.remove('active');
+        carrouselMobile[index].classList.remove('active');
         index = 0;
-        carrousel[index].classList.add('active');
+        carrouselMobile[index].classList.add('active');
     }
 }
 // reculer dans le carrousel
-precedent.addEventListener('click',slidePrecedente);
+precedentMobile.addEventListener('click',slidePrecedente);
 
 function slidePrecedente(){
     // decrementer pour retourner au 1er element du tableau
     if (index > 0){
-        carrousel[index].classList.remove('active');
-        index --;
-        carrousel[index].classList.add('active');
+        carrouselMobile[index].classList.remove('active');
+        index--;
+        carrouselMobile[index].classList.add('active');
     }
     else if (index === 0){
-        carrousel[index].classList.remove('active');
+        carrouselMobile[index].classList.remove('active');
         index = 3;
-        carrousel[index].classList.add('active');
+        carrouselMobile[index].classList.add('active');
     }
 }
-// fin partie CARROUSEL
+// fin partie CARROUSEL MOBILE
+
+// Carrousel DESKTOP
+
+// let index = 0;
+// suivantDesktop('click', slideSuivante);
+
+// function slideSuivante(){}
